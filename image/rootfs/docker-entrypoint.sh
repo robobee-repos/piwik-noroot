c85b0232df273f6f71c80055fc7f7e825069adf1
Erwin Müller <erwin.mueller@deventm.org>
2017-06-23 01:20:36 +0300

    Add php vars.

--- image/rootfs/docker-entrypoint.sh
+++ image/rootfs/docker-entrypoint.sh
@@ -1,41 +1,55 @@
 #!/bin/bash
 set -xe
 
+function check_files_exists() {
+  ls "$1" 1> /dev/null 2>&1
+}
+
 function copy_file() {
   file="$1"; shift
-  dir="$2"; shift
+  dir="$1"; shift
+  mod="$1"; shift
   if [ -e "$file" ]; then
     cp "$file" $dir/"$file"
-    chmod 0644 $dir/"$file"
+    chmod $mod $dir/"$file"
   fi
 }
 
-function copy_php() {
-  cd /php-in
-  copy_file php.ini /usr/local/etc/php/
+function copy_php_conf() {
+  dir="/php-in"
+  if [ ! -d "${dir}" ]; then
+    return
+  fi
+  cd "${dir}"
+  if ! check_files_exists "*.ini"; then
+    return
+  fi
+  rsync -v "${dir}/*.ini" /usr/local/etc/php/conf.d/
 }
 
-function copy_php_confd() {
-  cd /php-confd-in
-  rsync /php-confd-in/. /usr/local/etc/php/conf.d/
+function copy_php_fpm_conf() {
+  dir="/php-fpm-in"
+  if [ ! -d "${dir}" ]; then
+    return
+  fi
+  cd "${dir}"
+  if ! check_files_exists "*.conf"; then
+    return
+  fi
+  rsync -v "${dir}/*.conf" /usr/local/etc/php-fpm.d/
 }
 
 function sync_piwik() {
   cd $WEB_ROOT
-  rsync -v -rlD /usr/src/piwik/. .
+  if ! [ -e "$WEB_ROOT/piwik.php" ]; then
+    return
+  fi
+  rsync -rlD -v /usr/src/piwik/. .
 }
 
-if [ -d /php-in ]; then
-  copy_php
-fi
-
-if [ -d /php-confd-in ]; then
-  copy_php_confd
-fi
-
-if [ -e "$WEB_ROOT/piwik.php" ]; then
-  sync_piwik
-fi
+copy_php_conf
+copy_php_fpm_conf
+sync_piwik
 
 cd "$WEB_ROOT"
 echo "Running as `id`"
